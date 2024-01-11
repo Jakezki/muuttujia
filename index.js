8a47709bc1b099f1f37b9c63c12c5d0d8905f826
Jakezki <145100021+Jakezki@users.noreply.github.com>
2024-01-11 21:31:21 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function laskeKierrostenPisteet(kierros1, kierros2, kierros3) {
+  const yhteispisteet = kierros1 + kierros2 + kierros3;
+  const lopetusteksti = "Kierrosten yhteispistemäärä on: ";
+  console.log(lopetusteksti + yhteispisteet);
+}
+
+laskeKierrostenPisteet(10, 20, 15);
